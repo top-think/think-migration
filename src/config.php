efead98edbd5add2d9529ddd65a7891b59c53296
yunwuxin <448901948@qq.com>
2016-12-18 21:46:47 +0800

    注册回滚断点命令

--- src/config.php
+++ src/config.php
@@ -11,6 +11,7 @@
     "think\\migration\\command\\migrate\\Create",
     "think\\migration\\command\\migrate\\Run",
     "think\\migration\\command\\migrate\\Rollback",
+    "think\\migration\\command\\migrate\\Breakpoint",
     "think\\migration\\command\\migrate\\Status",
     "think\\migration\\command\\seed\\Create",
     "think\\migration\\command\\seed\\Run",
