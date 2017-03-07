b38331143668ab1d5e19f21d566da53ed09d27f7
yunwuxin <448901948@qq.com>
2017-03-07 23:45:21 +0800

    Table添加几个快捷方法

--- src/db/Table.php
+++ src/db/Table.php
@@ -15,15 +15,58 @@ use Phinx\Db\Table\Column;
 
 class Table extends \Phinx\Db\Table
 {
+    /**
+     * 设置id
+     * @param $id
+     * @return $this
+     */
     public function setId($id)
     {
         $this->options['id'] = $id;
         return $this;
     }
 
+    /**
+     * 设置主键
+     * @param $key
+     * @return $this
+     */
     public function setPrimaryKey($key)
     {
         $this->options['primary_key'] = $key;
+        return $this;
+    }
+
+    /**
+     * 设置引擎
+     * @param $engine
+     * @return $this
+     */
+    public function setEngine($engine)
+    {
+        $this->options['engine'] = $engine;
+        return $this;
+    }
+
+    /**
+     * 设置表注释
+     * @param $comment
+     * @return $this
+     */
+    public function setComment($comment)
+    {
+        $this->options['comment'] = $comment;
+        return $this;
+    }
+
+    /**
+     * 设置排序比对方法
+     * @param $collation
+     * @return $this
+     */
+    public function setCollation($collation)
+    {
+        $this->options['collation'] = $collation;
         return $this;
     }
 
