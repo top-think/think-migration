9188db6bc132fb0265dcf989f8a1310549d67a97
yunwuxin <448901948@qq.com>
2017-03-28 12:44:48 +0800

    调整注释

--- src/Migrator.php
+++ src/Migrator.php
@@ -15,6 +15,11 @@ use think\migration\db\Table;
 
 class Migrator extends AbstractMigration
 {
+    /**
+     * @param string $tableName
+     * @param array  $options
+     * @return Table
+     */
     public function table($tableName, $options = [])
     {
         return new Table($tableName, $options, $this->getAdapter());
