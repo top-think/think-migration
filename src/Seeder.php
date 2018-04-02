0f01f6e66fcfb6fd00b36d155cdcdab515f5cc49
军军呀 <1039845982@qq.com>
2018-04-02 11:07:08 +0800

    修正table() 返回对象 应为 think\migration\db\Table

--- src/Seeder.php
+++ src/Seeder.php
@@ -11,8 +11,12 @@
 namespace think\migration;
 
 use Phinx\Seed\AbstractSeed;
+use think\migration\db\Table;
 
 class Seeder extends AbstractSeed
 {
-
+    public function table($tableName, $options = [])
+    {
+        return new Table($tableName, $options, $this->getAdapter());
+    }
 }
