f9555743333ee9fe909d8580195c12cb87187a19
shouder <386734307@qq.com>
2019-08-26 23:50:22 +0800

    添加了AdapterInterface中的PHINX_TYPEPE_SMALL_INTEGER='smallinteger常量'

--- phinx/src/Phinx/Db/Adapter/AdapterInterface.php
+++ phinx/src/Phinx/Db/Adapter/AdapterInterface.php
@@ -47,6 +47,7 @@ interface AdapterInterface
     const PHINX_TYPE_CHAR           = 'char';
     const PHINX_TYPE_TEXT           = 'text';
     const PHINX_TYPE_INTEGER        = 'integer';
+    const PHINX_TYPE_SMALL_INTEGER  = 'smallinteger';
     const PHINX_TYPE_BIG_INTEGER    = 'biginteger';
     const PHINX_TYPE_FLOAT          = 'float';
     const PHINX_TYPE_DECIMAL        = 'decimal';
