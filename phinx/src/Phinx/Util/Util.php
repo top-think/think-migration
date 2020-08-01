5717d9e5f3ea745f6dbfd1e30b4402aaadff9a79
于破熊 <8553151@qq.com>
2020-08-01 12:09:06 +0800

    fix: 修复Util类中implode函数调用报错的问题

--- phinx/src/Phinx/Util/Util.php
+++ phinx/src/Phinx/Util/Util.php
@@ -106,7 +106,7 @@ class Util
     {
         $arr = preg_split('/(?=[A-Z])/', $className);
         unset($arr[0]); // remove the first element ('')
-        $fileName = static::getCurrentTimestamp() . '_' . strtolower(implode($arr, '_')) . '.php';
+        $fileName = static::getCurrentTimestamp() . '_' . strtolower(implode('_', $arr)) . '.php';
         return $fileName;
     }
 
